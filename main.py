12d98f7407044a484c344ceafac790c8cbe73306
Sarah Zartman <hobothulhu@gmail.com>
2022-02-25 11:06:19 -0600

    Initial test with SQLite

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # Initial main file
 
+# SQLITE database functionality to open and manipulate .sqlite files
+import sqlite3
+
+
+con = sqlite3.connect('sqlite-sakila.db')
+cur = con.cursor()
+cur.execute("SELECT * FROM actor")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
